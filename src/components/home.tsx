f1e02614dc62812151377e40371d8b7d8493de53
samipourquoi <sami.guiderdoni@gmail.com>
2021-03-12 23:08:53 +0100

    Add home page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+
+// @ts-ignore
+import logo from "../assets/logo.png"
 import "../styles/home.scss";
 
 export function Home() {
 	return (
 		<main>
-			<h1>World</h1>
+			<article>
+				<div className="icon">
+					<img alt="hypnos logo" src={logo}/>
+				</div>
+
+        <iframe src="https://discord.com/widget?id=626974236753264664&amp;theme=dark"
+                frameBorder="0"
+                sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"/>
+			</article>
 		</main>
 	);
 }
