c0344dc8838a59719aea9fa1ee89e4941fa018c1
samipourquoi <sami.guiderdoni@gmail.com>
2021-03-13 00:16:11 +0100

    Add Header styling

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-
 // @ts-ignore
-import logo from "../assets/logo.png"
+import banner from "../assets/banner.png"
 import "../styles/home.scss";
 
 export function Home() {
 	return (
 		<main>
 			<article>
-				<div className="icon">
-					<img alt="hypnos logo" src={logo}/>
+				<div className="banner">
+					<img alt="hypnos banner" src={banner}/>
 				</div>
 
         <iframe src="https://discord.com/widget?id=626974236753264664&amp;theme=dark"
