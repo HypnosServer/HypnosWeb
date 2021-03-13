d3d003fbd0d2f047cba9f4ef10ec88bbf1a572cd
samipourquoi <sami.guiderdoni@gmail.com>
2021-03-13 13:15:06 +0100

    Add text in the about page

--- src/web/components/about/index.tsx
+++ src/web/components/about/index.tsx
@@ -12,14 +12,19 @@ export function About() {
 
   return (
     <div className="members">
+      <p>We are a Minecraft technical server running 1.12.2 that started in October 2019. Our goal, like any other tech
+         server, is to make farms for just about everything and make them as best we can. We both dig perimeters and
+         dupe perimeters.<br/>
+         Applications are currently closed, if you wish to join please be friendly in the discord server and you will
+         be offered a chance of membership if you show interest.</p>
+
+      <h2>Members:</h2>
       <section className="member-list">
           {members.map(member =>
-            // <div className="member-container">
-              <MemberWidget
-                key={member.avatar}
-                {...member}
-              />
-            // </div>
+            <MemberWidget
+              key={member.avatar}
+              {...member}
+            />
           )}
       </section>
     </div>
